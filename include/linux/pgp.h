5c9123356f2d7221041e20db93375d099ac12717
David Howells <dhowells@redhat.com>
2012-05-22 22:55:56 +0100

    PGPLIB: Basic packet parser
    Provide a simple parser that extracts the packets from a PGP packet blob and
    passes the desirous ones to the given processor function:
    
    	struct pgp_parse_context {
    		u64 types_of_interest;
    		int (*process_packet)(struct pgp_parse_context *context,
    				      enum pgp_packet_tag type,
    				      u8 headerlen,
    				      const u8 *data,
    				      size_t datalen);
    	};
    
    	int pgp_parse_packets(const u8 *data, size_t datalen,
    			      struct pgp_parse_context *ctx);
    
    This is configured on with CONFIG_PGP_LIBRARY.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- include/linux/pgp.h
+++ include/linux/pgp.h
@@ -203,4 +203,29 @@ struct pgp_key_v4_packet {
 	u8 key_material[0];
 } __packed;
 
+/*
+ * PGP library packet parser
+ */
+struct pgp_parse_context {
+	u64 types_of_interest;
+	int (*process_packet)(struct pgp_parse_context *context,
+			      enum pgp_packet_tag type,
+			      u8 headerlen,
+			      const u8 *data,
+			      size_t datalen);
+};
+
+extern int pgp_parse_packets(const u8 *data, size_t datalen,
+			     struct pgp_parse_context *ctx);
+
+struct pgp_parse_pubkey {
+	enum pgp_key_version version : 8;
+	enum pgp_pubkey_algo pubkey_algo : 8;
+	time_t		creation_time;
+	time_t		expires_at;
+};
+
+extern int pgp_parse_public_key(const u8 **_data, size_t *_datalen,
+				struct pgp_parse_pubkey *pk);
+
 #endif /* _LINUX_PGP_H */
