23a58dea566358f12fdb8fc8d2737becd4c440c1
David Howells <dhowells@redhat.com>
2012-08-15 12:10:46 +0100

    KEYS: PGP data parser
    Implement a PGP data parser for the crypto key type to use when instantiating a
    key.
    
    This parser attempts to parse the instantiation data as a PGP packet sequence
    (RFC 4880) and if it parses okay, attempts to extract a public-key algorithm
    key or subkey from it.
    
    If it finds such a key, it will set up a public_key subtype payload with
    appropriate handler routines (DSA or RSA) and attach it to the key.
    
    Thanks to Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp> for pointing out
    some errors.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

---
+++ security/keys/crypto/pgp_parser.h
@@ -0,0 +1,23 @@
+/* PGP crypto data parser internal definitions
+ *
+ * Copyright (C) 2011 Red Hat, Inc. All Rights Reserved.
+ * Written by David Howells (dhowells@redhat.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public Licence
+ * as published by the Free Software Foundation; either version
+ * 2 of the Licence, or (at your option) any later version.
+ */
+
+#include <linux/pgp.h>
+
+#define kenter(FMT, ...) \
+	pr_devel("==> %s("FMT")\n", __func__, ##__VA_ARGS__)
+#define kleave(FMT, ...) \
+	pr_devel("<== %s()"FMT"\n", __func__, ##__VA_ARGS__)
+
+/*
+ * pgp_key_parser.c
+ */
+extern const
+struct public_key_algorithm *pgp_public_key_algorithms[PGP_PUBKEY__LAST];
