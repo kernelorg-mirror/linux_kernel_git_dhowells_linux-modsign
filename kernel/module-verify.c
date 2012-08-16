e5d89667511a41d9da392b22199d80a37f0174b5
David Howells <dhowells@redhat.com>
2012-08-16 15:19:12 +0100

    MODSIGN: Panic the kernel if FIPS is enabled upon module signing failure
    If module signing fails when the kernel is running with FIPS enabled then the
    kernel should panic lest the crypto layer be compromised.  Possibly a panic
    shouldn't happen on cases like ENOMEM.
    
    Reported-by: Stephan Mueller <stephan.mueller@atsec.com>
    Signed-off-by: David Howells <dhowells@redhat.com>

--- kernel/module-verify.c
+++ kernel/module-verify.c
@@ -20,6 +20,7 @@
 #include <linux/err.h>
 #include <linux/modsign.h>
 #include <linux/moduleparam.h>
+#include <linux/fips.h>
 #include <keys/crypto-type.h>
 #include "module-verify.h"
 
@@ -97,6 +98,10 @@ int module_verify(const void *data, size_t size, bool *_gpgsig_ok)
 	ret = module_verify_signature(data, size);
 
 	pr_devel("module_verify_signature() = %d\n", ret);
+
+        if (ret < 0 && fips_enabled)
+                panic("Module verification failed with error %d in FIPS mode\n",
+                      ret);
 
 	switch (ret) {
 	case 0:			/* Good signature */
