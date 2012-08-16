e6f0140735e48abcb2087661421549dbee31bc37
David Howells <dhowells@redhat.com>
2012-08-16 15:19:09 +0100

    MODSIGN: Provide module signing public keys to the kernel
    Include a PGP keyring containing the public keys required to perform module
    verification in the kernel image during build and create a special keyring
    during boot which is then populated with keys of crypto type holding the public
    keys found in the PGP keyring.
    
    These can be seen by root:
    
    [root@andromeda ~]# cat /proc/keys
    07ad4ee0 I-----     1 perm 3f010000     0     0 crypto    modsign.0: RSA 87b9b3bd []
    15c7f8c3 I-----     1 perm 1f030000     0     0 keyring   .module_sign: 1/4
    ...
    
    It is probably worth permitting root to invalidate these keys, resulting in
    their removal and preventing further modules from being loaded with that key.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

---
+++ kernel/module-verify.h
@@ -0,0 +1,14 @@
+/* Module verification definitions
+ *
+ * Copyright (C) 2004, 2012 Red Hat, Inc. All Rights Reserved.
+ * Written by David Howells (dhowells@redhat.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public Licence
+ * as published by the Free Software Foundation; either version
+ * 2 of the Licence, or (at your option) any later version.
+ */
+
+#ifdef CONFIG_MODULE_SIG
+extern struct key *modsign_keyring;
+#endif
