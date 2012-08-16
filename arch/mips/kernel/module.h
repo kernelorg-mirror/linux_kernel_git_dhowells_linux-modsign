c6cfa3260f1e2961e69a2e3240695954aed24976
Ralf Baechle <ralf@linux-mips.org>
2012-08-16 01:38:43 +0100

    MIPS: Fix module.c build for 32 bit
    Fixes build failure introduced by "Make most arch asm/module.h files use
    asm-generic/module.h" by moving all the RELA processing code to a
    separate file to be used only for RELA processing on 64-bit kernels.
    
      CC      arch/mips/kernel/module.o
    arch/mips/kernel/module.c:250:14: error: 'reloc_handlers_rela' defined but not
    used [-Werror=unused-variable]
    cc1: all warnings being treated as errors
    
    make[6]: *** [arch/mips/kernel/module.o] Error 1
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: David Howells <dhowells@redhat.com>

---
+++ arch/mips/kernel/module.h
@@ -0,0 +1,12 @@
+/* Internal definitions for MIPS module code
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public Licence
+ * as published by the Free Software Foundation; either version
+ * 2 of the Licence, or (at your option) any later version.
+ */
+
+/*
+ * module.c
+ */
+extern int apply_r_mips_none(struct module *me, u32 *location, Elf_Addr v);
